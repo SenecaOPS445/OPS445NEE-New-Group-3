bb3be36583a1d852b4b78a79abd45801228042ff
Haiwei Liu <hliu232@myseneca.ca>
2025-03-23 14:55:51 -0400

    Initial commit

---
+++ assignment2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Project: Network Configuration & Subnet Calculator
+Group: 3
+SenecaID: hliu232
+
+Description:
+This script calculates subnet details including network address, IP range, broadcast address, and more, 
+based on a given IP address and subnet mask.
+'''
+import ipaddress
+
+def calculate_subnet(ip, mask):
+    """Calculates subnet details from IP and mask."""
+
+
+
+
+if __name__ == "__main__":
+    
+    ''''''
