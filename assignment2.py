52208e0069e2b57caefad1a2a455721faf2938ae
Jhanlyn Brita Dannuy <166140909+jdannuy@users.noreply.github.com>
2025-04-05 17:02:47 -0400

    Update main V 1.0 (#7)
    * mergin-fix
    
    * Final release of version 1.0
    
    ---------
    
    Co-authored-by: Haiwei Liu <hliu232@myseneca.ca>

--- assignment2.py
+++ assignment2.py
@@ -6,12 +6,8 @@ This script calculates subnet details including network address, IP range, broad
 based on a given IP address and subnet mask.
 '''
 
-import argparse
-import ipaddress
-
 # Group Information
 group_info = """
-
 ===================================
 Group 3
 Network Configuration and Subnetting
@@ -20,6 +16,9 @@ Jhanlyn Brita Dannuy (jdannuy)
 Haiwei Liu (hliu232)
 ===================================
 """
+
+import argparse
+import ipaddress
 
 # Step 1: Setting up argparse for optional arguments
 def setup_argparse():
